b783b3502e6f414fb2cd7ced3707db00f04e3f9a
Ayush <kumarayush9694@gmail.com>
2024-07-28 15:55:08 +0530

    feat: Login Page

---
+++ src/components/providers.tsx
@@ -0,0 +1,17 @@
+import React, { Children } from 'react'
+import { Toaster } from 'react-hot-toast'
+
+interface ProviderProps{
+    children:React.ReactNode
+}
+
+const Providers = ({children}:ProviderProps) => {
+  return (
+    <>
+    <Toaster position='top-center' reverseOrder ={false}/>
+    {children}
+    </>
+  )
+}
+
+export default Providers
